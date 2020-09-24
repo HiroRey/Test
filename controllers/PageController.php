d725b89f99367e00585c9106f55ddb09b8258d29
HiroRey <53913499+HiroRey@users.noreply.github.com>
2020-09-24 13:48:17 +0300

    Update PageController.php

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -8,9 +8,12 @@ use app\models\Page;
 
 class PageController extends \yii\web\Controller
 {
+    $nfjfoe = 1214124;
+    
     public function actionIndex()
     {
         $page = Page::find()->all();
+        
 
 
         return $this->render('index', ['pages' => $page]);
